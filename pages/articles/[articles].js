aabc5b4209bad7d23985e5704475b795b255b94c
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-03-29 12:24:20 +0200

    fin exo top

--- pages/articles/[articles].js
+++ pages/articles/[articles].js
@@ -1,23 +1,35 @@
-// import React from "react";
-// import styles from "@/styles/Home.module.css";
-// import { v4 as uuidv4 } from "uuid";
-// import { useRouter } from "next/router";
-
 export default function articles(props) {
   // console.log(props);
 
   //   const router = useRouter();
 
   return (
-    <div className="container">
-      <div className="card-deck">
-        <h5 className="card-title">{props.articleEnCours.name}</h5>
-        <p>username: {props.articleEnCours.username} </p>
-        <div className="card-deck">
-          <p>Username: {props.articleEnCours.username} </p>
-          <p>Email: {props.articleEnCours.email} </p>
-          <p>Site web: {props.articleEnCours.website} </p>
-          <p>Téléphone: {props.articleEnCours.phone} </p>
+    <div className="container px-4 pt-3">
+      <h2 className="text-center mb-4 mt-5">{props.articleEnCours.name}</h2>
+
+      <div className="row justrify-content-center">
+        <div className="col-12 col-lg8-col-xl-6">
+          <div className="card p-2">
+            <div className="card-body">
+              <div className="card-title">
+                <h4> {props.articleEnCours.name}</h4>
+                <h5 className="card-subtitle mb-2 text-muted">
+                  Username: {props.articleEnCours.username}
+                </h5>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Email: {props.articleEnCours.email}
+                  </li>
+                  <li className="list-group-item">
+                    Site web: {props.articleEnCours.website}
+                  </li>
+                  <li className="list-group-item">
+                    Téléphone: {props.articleEnCours.phone}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -35,6 +47,7 @@ export async function getStaticProps(context) {
   //data renvoie bien les noms des users
   // console.log(data, "log de data staticprops------------------------");
 
+  // si on veut voir l'exemple avec l'Id, voir page [posts]
   const articleEnCours = data.find((item) => item.name === slug);
 
   return {
@@ -54,6 +67,7 @@ export async function getStaticPaths() {
   //   console.log(data, "log de data------------------------");
 
   const paths = data.map((item) => ({
+    // on pourrait utiliser l'id (Cf. posts)
     params: { articles: item.name },
   }));
 
